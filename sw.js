53254f2fc093b818a3292b15b87f3f7227c5f907
MGARCIA-85 <massimo.garcia85@gmail.com>
2026-05-21 21:52:13 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'billares-v188';
+const CACHE = 'billares-v190';
 const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
-self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => { e.respondWith(fetch(e.request, {cache:'no-store'}).catch(() => caches.match(e.request))); });
