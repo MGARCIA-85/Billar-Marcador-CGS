7d36d1d6048565bb8ae573476a061abae114a018
MGARCIA-85 <massimo.garcia85@gmail.com>
2026-05-30 19:34:08 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'billares-v313';
-const FILES = ['/index.html'];
+const CACHE = 'billares-v310';
+const FILES = ['/index.html', '/sw.js'];
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
@@ -11,11 +11,8 @@ self.addEventListener('activate', e => {
   );
 });
 self.addEventListener('fetch', e => {
-  const url = e.request.url;
-  if (!url.startsWith('http')) return;
   e.respondWith(
     fetch(e.request).then(res => {
-      if (!res || res.status !== 200 || res.type !== 'basic') return res;
       const clone = res.clone();
       caches.open(CACHE).then(c => c.put(e.request, clone));
       return res;
