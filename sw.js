dda429ac0ef7b4784f21c67fdb336c36b61ec374
MGARCIA-85 <massimo.garcia85@gmail.com>
2026-06-05 19:54:53 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,5 @@
-const CACHE = 'billares-v365';
-const FILES = ['/index.html'];
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
-  );
-});
-self.addEventListener('fetch', e => {
-  const url = e.request.url;
-  if (!url.startsWith('http')) return;
-  e.respondWith(
-    fetch(e.request).then(res => {
-      if (!res || res.status !== 200 || res.type !== 'basic') return res;
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }).catch(() => caches.match(e.request))
-  );
-});
+const CACHE = 'gestor-cgs-v187';
+const FILES = ['./GestorPedidosCGS.html', './manifest.json'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
