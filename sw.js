a542b9fdcc9a73ed45fff2666b8efa4cb66b5b4c
MGARCIA-85 <massimo.garcia85@gmail.com>
2026-06-05 21:46:19 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'billares-v365';
+const FILES = ['/index.html'];
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  const url = e.request.url;
+  if (!url.startsWith('http')) return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      if (!res || res.status !== 200 || res.type !== 'basic') return res;
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
